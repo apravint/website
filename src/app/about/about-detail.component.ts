8c08a84fa49a0fe62c71e5126d01e7e307dc5d5c
Pravin <apravint@gmail.com>
2025-10-07 11:25:15 +0530

    Oct 7 changes

---
+++ src/app/about/about-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-detail',
+  standalone: true,
+  templateUrl: './about-detail.component.html',
+  styleUrls: ['./about-detail.component.scss']
+})
+export class AboutDetailComponent {}
---
+++ src/app/privacy/privacy-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-detail',
+  standalone: true,
+  templateUrl: './privacy-detail.component.html',
+  styleUrls: ['./privacy-detail.component.scss']
+})
+export class PrivacyDetailComponent {}
