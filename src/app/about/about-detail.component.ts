ff3fc812d05880992c82667c0bd9aba88358e7ba
Pravin <apravint@gmail.com>
2025-12-18 22:55:27 +0530

    feat: merge detail components into parents (home, about, privacy) and remove detail files; update routes

--- src/app/about/about-detail.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-about-detail',
-  standalone: true,
-  templateUrl: './about-detail.component.html',
-  styleUrls: ['./about-detail.component.scss']
-})
-export class AboutDetailComponent {}
--- src/app/privacy/privacy-detail.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-privacy-detail',
-  standalone: true,
-  templateUrl: './privacy-detail.component.html',
-  styleUrls: ['./privacy-detail.component.scss']
-})
-export class PrivacyDetailComponent {}
