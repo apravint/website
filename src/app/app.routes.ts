d1799ee92f0c0eb70031d9b1eca0745d7b8992d5
Pravin <apravint@gmail.com>
2025-12-18 23:41:15 +0530

    chore(routes): add /terms route and navbar link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PrivacyComponent } from './privacy/privacy.component';
+import { TermsComponent } from './terms/terms.component';
 
 export const routes: Routes = [
 	{ path: 'home', component: HomeComponent },
 	{ path: 'about', component: AboutComponent },
 	{ path: 'privacy', component: PrivacyComponent },
+	{ path: 'terms', component: TermsComponent },
 	{ path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
