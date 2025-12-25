711bfc2de09f76d054d509128cb293592ed42985
Pravin <apravint@gmail.com>
2025-12-25 17:15:29 +0530

    improved UI

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AdUnitComponent } from '../shared/ad-unit/ad-unit.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
+  imports: [AdUnitComponent],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {}
+export class AboutComponent { }
