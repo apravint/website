c4b008fe9e80d044b24bcdcabd2420b9f6460eba
Pravin <apravint@gmail.com>
2025-12-25 19:31:38 +0530

    test(home): provide RouterTestingModule and mock TranslationService for HomeComponent tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslationService } from '../shared/translation.service';
+import { of } from 'rxjs';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +11,13 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [RouterTestingModule, HomeComponent],
+      providers: [
+        { provide: TranslationService, useValue: { translate: (k: string) => {
+          if (k.includes('HOME.HERO.TITLE')) return 'Tamil Kavithai';
+          return k;
+        }, translations$: of({}) } }
+      ]
     })
     .compileComponents();
 
