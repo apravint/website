2aa749e4b8c405ebc295e347ec1f32edffca4321
Ayyappa Pravin <presspravin@outlook.com>
2026-09-14 22:26:56 +0530

    Add GitHub Actions auto-build workflow for Android App Bundle (.aab) and APK release

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pravintamilan.cyberracer',
+  appName: 'Cyber Racer 3D',
+  webDir: 'out'
+};
+
+export default config;
